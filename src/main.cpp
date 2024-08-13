82d60285ebb773a66b9101b413e91c477636665c
Alexander Yushenko <alexander.iushchenko@glidewelldental.com>
2024-08-13 12:05:21 +0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+
+#include <sol/sol.hpp>
+#include <cassert>
+
+int main()
+{
+    sol::state lua;
+    int x = 0;
+    lua.set_function( "beep", [&x] { ++x; } );
+    lua.script( "beep()" );
+    assert( x == 1 );
+}
